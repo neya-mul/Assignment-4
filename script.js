89608c25d635e727b897825fab17a575678ba058
neya-mul <neyamulislam946@gmail.com>
2026-02-21 21:40:33 +0600

    job count completed

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello js');
+let totalJobs = document.getElementById('total-jobs');
+let totalJobsValue = totalJobs.innerText 
+let availableJobs = document.getElementById('available-jobs');
+let allJobs = document.getElementById('all-jobs');
+
+
+let allJobsLength = allJobs.children.length;
+availableJobs.innerText = allJobsLength
+
+totalJobs.innerText = allJobsLength;
