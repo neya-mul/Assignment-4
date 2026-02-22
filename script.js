960fee5bfce7753dbcefab5ebbc9652eb9fdda24
neya-mul <neyamulislam946@gmail.com>
2026-02-22 08:57:00 +0600

    buttons color will change now

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
 let totalJobs = document.getElementById('total-jobs');
-let totalJobsValue = totalJobs.innerText 
+let totalJobsValue = totalJobs.innerText
 let availableJobs = document.getElementById('available-jobs');
 let allJobs = document.getElementById('all-jobs');
-
-
 let allJobsLength = allJobs.children.length;
-availableJobs.innerText = allJobsLength
-
-totalJobs.innerText = allJobsLength;
 
 
 let allInterView = document.getElementById('all-interview');
 let allInterViewValue = allInterView.innerText;
 let allRejections = document.getElementById('all-rejections');
 let allRejectionsValues = allRejections.innerText;
+
+let all = document.getElementById('all')
+let interview = document.getElementById('interview')
+let rejected = document.getElementById('rejected');
+
+
+availableJobs.innerText = allJobsLength
+totalJobs.innerText = allJobsLength;
+
+all.addEventListener('click', function () {
+    interview.style.backgroundColor = 'white'
+    interview.style.color = 'black'
+    rejected.style.backgroundColor = 'white'
+    rejected.style.color = 'black'
+    all.style.backgroundColor = '#3B82F6'
+    all.style.color = 'white'
+
+})
+
+interview.addEventListener('click', function () {
+    interview.style.backgroundColor = '#3B82F6'
+    interview.style.color = 'white'
+    all.style.backgroundColor = 'white'
+    all.style.color = 'black'
+    rejected.style.backgroundColor = 'white'
+    rejected.style.color = 'black'
+
+})
+
+rejected.addEventListener('click', function () {
+    interview.style.backgroundColor = 'white'
+    interview.style.color = 'black'
+    all.style.backgroundColor = 'white'
+    all.style.color = 'black'
+    rejected.style.backgroundColor = '#3B82F6'
+    rejected.style.color = 'white'
+
+})
+
+
 
