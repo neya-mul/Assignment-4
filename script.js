c478465937321e5bcf536702686d9d90021e2e73
neya-mul <neyamulislam946@gmail.com>
2026-02-24 07:18:22 +0600

    some part has completed

--- script.js
+++ script.js
@@ -58,12 +58,17 @@ function buttonColorChange(id) {
         allJobs.classList.remove('hidden')
         hiddenSection.classList.add('hidden')
     }
+    else if (id == 'rejected') {
+        allJobs.classList.add('hidden')
+        hiddenSection.classList.remove('hidden', 'p-15', 'text-center')
+    }
 
 }
 
 
 mainContainer.addEventListener('click', function (event) {
-    console.log(event.target.classList.contains('interview'))
+    // console.log(event.target.classList.contains('interview'))
+
     if (event.target.classList.contains('interview')) {
         let parentNode = event.target.parentNode
         let jobTitle = parentNode.querySelector('.job-title').innerText
@@ -100,13 +105,54 @@ mainContainer.addEventListener('click', function (event) {
         if (!card) {
             interviewList.push(wholeCard);
 
-            console.log(interviewList);
+            // console.log(interviewList);
 
         }
         allJobsCount()
         pushJobs()
     }
+    else if (event.target.classList.contains('rejected')) {
+        let parentNode = event.target.parentNode
+        let jobTitle = parentNode.querySelector('.job-title').innerText
+        let deletBtn = parentNode.querySelector('.delete-btn').innerText
+        let title = parentNode.querySelector('.title').innerText
+        let status = parentNode.querySelector('.status').innerText
+        let time = parentNode.querySelector('.time').innerText
+        let salary = parentNode.querySelector('.salary').innerText
+        let badge = parentNode.querySelector('.badge').innerText
+        let description = parentNode.querySelector('.description').innerText
+        let interview = parentNode.querySelector('.interview').innerText
+        let rejected = parentNode.querySelector('.rejected').innerText
 
+        parentNode.querySelector('.badge').innerText = 'Rejected'
+        parentNode.querySelector('.badge').classList.add('border', 'border-green-300', 'text-green-500', 'bg-white', 'p-2')
+
+
+
+        let wholeCard = {
+            jobTitle,
+            deletBtn,
+            title,
+            status,
+            time,
+            salary,
+            badge,
+            description,
+            interview,
+            rejected
+        }
+
+        let card = rejectionList.find(element => element.jobTitle === wholeCard.jobTitle)
+
+        if (!card) {
+            rejectionList.push(wholeCard);
+
+            // console.log(interviewList);
+
+        }
+        allJobsCount()
+        rejectedJobs()
+    }
 
 })
 
@@ -115,7 +161,7 @@ mainContainer.addEventListener('click', function (event) {
 function pushJobs() {
     hiddenSection.innerHTML = '';
     for (let interviews of interviewList) {
-        console.log(interviews);
+        // console.log(interviews);
 
         let div = document.createElement('div');
         div.classList.add('job', 'p-4', 'border', 'border-gray-300', 'mt-7', 'bg-white', 'rounded', 'space-y-2')
@@ -132,10 +178,46 @@ function pushJobs() {
                     <li class="salary">${interviews.salary}</li>
                 </div>
                 <span class=" badge bg-white p-2 border border-green-300 text-green-500 rounded not-applied">Interview</span>
-                <p class="mt-5 description">${interviews.title}.</p>
+                <p class="mt-5 description">${interviews.description}.</p>
                 <button
                     class="border border-green-300 text-green-500 p-2 rounded font-bold interview">${interviews.interview}</button>
                 <button class="border border-red-300 text-red-500 rounded p-2 font-bold rejected">${interviews.rejected}</button>
+                
+
+    
+    `
+        hiddenSection.appendChild(div)
+    }
+}
+
+
+
+
+
+function rejectedJobs() {
+    hiddenSection.innerHTML = '';
+    for (let rejections of rejectionList) {
+        // console.log(interviews);
+
+        let div = document.createElement('div');
+        div.classList.add('job', 'p-4', 'border', 'border-gray-300', 'mt-7', 'bg-white', 'rounded', 'space-y-2')
+        div.innerHTML = `
+
+                 <div class="flex justify-between">
+                    <h1 class="job-title text-2xl ">${rejections.jobTitle}</h1>
+                    <button><i class="fa-solid fa-trash-can delete-btn"></i></button>
+                </div>
+                <p class="title">${rejections.title}</p>
+                <div class="flex gap-7 my-4">
+                    <p class="status">${rejections.status}</p>
+                    <li class="time">${rejections.time}</li>
+                    <li class="salary">${rejections.salary}</li>
+                </div>
+                <span class=" badge bg-white p-2 border border-red-300 text-red-500 rounded not-applied">Rejected</span>
+                <p class="mt-5 description">${rejections.description}.</p>
+                <button
+                    class="border border-green-300 text-green-500 p-2 rounded font-bold interview">${rejections.interview}</button>
+                <button class="border border-red-300 text-red-500 rounded p-2 font-bold rejected">${rejections.rejected}</button>
                 
 
     
