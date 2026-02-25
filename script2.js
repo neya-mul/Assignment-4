99dd0a60e43493fc19bf995a79f02c392ec8602e
neya-mul <neyamulislam946@gmail.com>
2026-02-25 16:30:23 +0600

    section counts updated

--- script2.js
+++ script2.js
@@ -1,33 +1,16 @@
 let allSection = 'all';
 let selectedSection = ['bg-blue-500', 'text-white']
 let notSelectedSection = ['bg-white', 'text-black']
-
 let allJobs = document.getElementById('all-jobs');
 let interviewSection = document.getElementById('interview-section')
 let rejectionSection = document.getElementById('rejection-section')
-
-
-let interviewHiddenSection = document.getElementById('interview-hidden')
-let rejectionHiddenSection = document.getElementById('rejection-hidden')
-
+let noJobs = document.getElementById('no-jobs')
 // jobs count
 let totalJobs = document.getElementById('total-jobs')
 let allInterviews = document.getElementById('all-interviews')
 let allRejections = document.getElementById('all-rejections')
 let availableJobs = document.getElementById('available-jobs')
-
-
 let allJobsLength = allJobs.children.length
-
-
-// length count 
-totalJobs.innerText = allJobsLength;
-availableJobs.innerText = allJobsLength;
-
-
-
-
-
 function sectionChange(eachSection) {
     // console.log(button);
     let sections = ['all', 'interview', 'rejected'];
@@ -36,6 +19,7 @@ function sectionChange(eachSection) {
         // console.log(section);
         let clickedSection = document.getElementById(section)
         if (section === eachSection) {
+
             clickedSection.classList.add(...selectedSection)
             clickedSection.classList.remove(...notSelectedSection)
 
@@ -47,59 +31,88 @@ function sectionChange(eachSection) {
 
     }
 
+let pages = [allJobs, interviewSection, rejectionSection];
+for(let page of pages){
+    page.classList.add('hidden')
+}
+noJobs.classList.add('hidden')
     if (eachSection === 'all') {
         allJobs.classList.remove('hidden')
+        if(allJobs.children.length < 1){
+            noJobs.classList.remove('hidden')
+
+        }
     }
     else if (eachSection === 'interview') {
         interviewSection.classList.remove('hidden')
-        allJobs.classList.add('hidden')
-        rejectionSection.classList.add('hidden')
+        if(interviewSection.children.length < 1){
+                noJobs.classList.remove('hidden')
+
+        }
+        // allJobs.classList.add('hidden')
+        // rejectionSection.classList.add('hidden')
     }
     else {
-        allJobs.classList.add('hidden')
-        interviewSection.classList.add('hidden')
+        // allJobs.classList.add('hidden')
+        // interviewSection.classList.add('hidden')
         rejectionSection.classList.remove('hidden')
+        if(rejectionSection.children.length < 1){
+              noJobs.classList.remove('hidden')
+
+        }
     }
 
 }
 sectionChange(allSection)
 
-
-
 document.getElementById('all-sections').addEventListener('click', function (event) {
-
     let clickedElement = event.target;
     let job = clickedElement.closest('.job');
     let notApplied = job.querySelector('.not-applied')
     let parent = job.parentNode
-
     // interview button 
     if (clickedElement.classList.contains('interview')) {
         // console.log('interview button clicked');
         interviewSection.appendChild(job)
         notApplied.innerText = 'Interview'
         notApplied.classList.add('bg-white', 'border', 'text-green-500', 'border-green-300')
-
-
+        counts()
     }
     // reject button
-    else if (clickedElement.classList.contains('rejected')) {
+    if (clickedElement.classList.contains('rejected')) {
         // console.log('rejected buttons clicked')
         rejectionSection.appendChild(job)
         notApplied.innerText = 'Rejected'
         notApplied.classList.add('bg-white', 'text-red-500', 'border', 'border-red-300')
-
+        counts()
     }
     // delete button
-    else if (clickedElement.classList.contains('delete-btn')) {
+    if (clickedElement.classList.contains('delete-btn')) {
         // console.log(dtlBtn)
         parent.removeChild(job)
-        totalJobs.innerText = allJobsLength - 1;
-        availableJobs.innerText = allJobsLength -1;
-
-
     }
 
-
-
 })
+
+
+
+function counts (){
+// length count 
+
+totalJobs.innerText = allJobsLength;
+allInterviews.innerText = interviewSection.children.length;
+allRejections.innerText = rejectionSection.children.length;
+
+// let count ={
+//     all: allJobsLength, 
+//     interview: interviewSection.children.length,
+//     rejected:  rejectionSection.children.length
+// }
+
+// totalJobs.innerText = count['all']
+// allInterviews.innerText = count['interview']
+// allRejections.innerText = count['rejected']
+
+
+}
+counts()
