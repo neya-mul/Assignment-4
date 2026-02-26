56ef9ba72834f0bbe8f05aa1460bfb0cce14c2f7
neya-mul <neyamulislam946@gmail.com>
2026-02-26 13:03:03 +0600

    section counts updated

--- script2.js
+++ script2.js
@@ -1,19 +1,23 @@
-let allSection = 'all';
+let allSections = 'all';
 let selectedSection = ['bg-blue-500', 'text-white']
 let notSelectedSection = ['bg-white', 'text-black']
 let allJobs = document.getElementById('all-jobs');
 let interviewSection = document.getElementById('interview-section')
 let rejectionSection = document.getElementById('rejection-section')
 let noJobs = document.getElementById('no-jobs')
+// let availableJobsCount = document.getElementById('available-jobs');
 // jobs count
 let totalJobs = document.getElementById('total-jobs')
 let allInterviews = document.getElementById('all-interviews')
 let allRejections = document.getElementById('all-rejections')
+
 let availableJobs = document.getElementById('available-jobs')
+
 let allJobsLength = allJobs.children.length
 function sectionChange(eachSection) {
     // console.log(button);
     let sections = ['all', 'interview', 'rejected'];
+    allSections = eachSection
 
     for (let section of sections) {
         // console.log(section);
@@ -31,39 +35,33 @@ function sectionChange(eachSection) {
 
     }
 
-let pages = [allJobs, interviewSection, rejectionSection];
-for(let page of pages){
-    page.classList.add('hidden')
-}
-noJobs.classList.add('hidden')
+    let pages = [allJobs, interviewSection, rejectionSection];
+    for (let page of pages) {
+        page.classList.add('hidden')
+    }
+    noJobs.classList.add('hidden')
     if (eachSection === 'all') {
         allJobs.classList.remove('hidden')
-        if(allJobs.children.length < 1){
+        if (allJobs.children.length < 1) {
             noJobs.classList.remove('hidden')
 
         }
     }
     else if (eachSection === 'interview') {
         interviewSection.classList.remove('hidden')
-        if(interviewSection.children.length < 1){
-                noJobs.classList.remove('hidden')
-
+        if (interviewSection.children.length < 1) {
+            noJobs.classList.remove('hidden')
         }
-        // allJobs.classList.add('hidden')
-        // rejectionSection.classList.add('hidden')
     }
     else {
-        // allJobs.classList.add('hidden')
-        // interviewSection.classList.add('hidden')
         rejectionSection.classList.remove('hidden')
-        if(rejectionSection.children.length < 1){
-              noJobs.classList.remove('hidden')
-
+        if (rejectionSection.children.length < 1) {
+            noJobs.classList.remove('hidden');
         }
     }
-
+    counts()
 }
-sectionChange(allSection)
+sectionChange(allSections)
 
 document.getElementById('all-sections').addEventListener('click', function (event) {
     let clickedElement = event.target;
@@ -90,29 +88,25 @@ document.getElementById('all-sections').addEventListener('click', function (even
     if (clickedElement.classList.contains('delete-btn')) {
         // console.log(dtlBtn)
         parent.removeChild(job)
+        counts()
+
     }
 
 })
 
+function counts() {
+    // length count 
 
+    let count = {
+        all: allJobsLength,
+        interview: interviewSection.children.length,
+        rejections: rejectionSection.children.length
+    };
 
-function counts (){
-// length count 
-
-totalJobs.innerText = allJobsLength;
-allInterviews.innerText = interviewSection.children.length;
-allRejections.innerText = rejectionSection.children.length;
-
-// let count ={
-//     all: allJobsLength, 
-//     interview: interviewSection.children.length,
-//     rejected:  rejectionSection.children.length
-// }
-
-// totalJobs.innerText = count['all']
-// allInterviews.innerText = count['interview']
-// allRejections.innerText = count['rejected']
-
-
+    totalJobs.innerText = count.all;
+    allInterviews.innerText = count.interview;
+    allRejections.innerText = count.rejections;
+    availableJobs.innerText = count[allSections]
 }
 counts()
+
